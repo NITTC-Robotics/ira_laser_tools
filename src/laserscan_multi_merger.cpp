d4745140349ac63518b99fe4b549e7bb1069860b
Nyarn <s16054@tokyo.kosen-ac.jp>
2020-07-05 23:04:20 +0900

    Update laserscan_multi_merger.cpp

--- src/laserscan_multi_merger.cpp
+++ src/laserscan_multi_merger.cpp
@@ -88,8 +88,8 @@ void LaserscanMerger::laserscan_topic_parser()
 			}
 		}
 	}*/
-	tmp_input_topics.push_back("/top_right_lidar");
-	tmp_input_topics.push_back("/lower_left_lidar");
+	tmp_input_topics.push_back("/pc2laserscan/top_right_lidar");
+	tmp_input_topics.push_back("/pc2laserscan/lower_left_lidar");
 
 	sort(tmp_input_topics.begin(),tmp_input_topics.end());
 	std::vector<string>::iterator last = std::unique(tmp_input_topics.begin(), tmp_input_topics.end());
